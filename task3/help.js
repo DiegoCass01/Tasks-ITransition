9bf6dadaba336a67ba049b70e8f97d2702f0fa2f
DiegoCass01 <diego.castillo170901@gmail.com>
2024-09-11 20:32:46 -0600

    better help interface

--- task3/help.js
+++ task3/help.js
@@ -2,19 +2,19 @@ class HelpTable {
   static printHelp(moves) {
     const n = moves.length;
     console.log("Help Table:");
-    console.log(" ".repeat(8) + moves.join("  "));
+    console.warn("PC\\USER | " + moves.join("  | "));
     for (let i = 0; i < n; i++) {
       let row = moves[i].padEnd(8);
       for (let j = 0; j < n; j++) {
         if (i === j) {
-          row += "Draw".padEnd(8);
+          row += "| Draw".padEnd(8);
         } else if (
           (j > i && j - i <= Math.floor(n / 2)) ||
           (j < i && i - j > Math.floor(n / 2))
         ) {
-          row += "Lose".padEnd(8);
+          row += "| Lose".padEnd(8);
         } else {
-          row += "Win".padEnd(8);
+          row += "| Win".padEnd(8);
         }
       }
       console.log(row);
